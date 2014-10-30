85b6b3df79e05c77e571510f96af1b57541a2d56
Dasheng Chen <habinacds@gmail.com>
2014-10-30 01:39:59 -0700

    naive version

---
+++ get-subm.py
@@ -0,0 +1,18 @@
+from subprocess import call
+from os import listdir
+from os.path import isfile, join
+
+roster = open("roster_file")
+logins = []
+#read logins
+for line in roster:
+    logins.append(line[:len(line) - 1])
+
+#create folder
+call(["mkdir", "submission"])
+for login in logins:
+    call(["get-subm", "proj1-1",login])
+    call(["mkdir", "submission/" + login])
+    call(["mv", "proj1_1A.txt", "submission/" + login])
+#compress submission
+# call(["tar", "zcvf", "archive.tar.gz", "submission/"])
